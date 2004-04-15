5b4791a5b876e15b8c9277f89805ba269863a095
(凸) <devnull@localhost>
2004-04-15 15:17:14 +0000

    //0939 by (凸) ・cutinパケットを0x145(ファイル名16文字)から0x1b3(64文字)に変更 ・ついでに雑談スレに上げたathena-startを同梱
    	(/)
    	athena-start saveファイルが無いときに作るように
    	(map/)
    	clif.c
    		clif_cutin() 本鯖パケット準拠に変更
    	(doc/)
    	client_packet.txt 修正

--- map/clif.c
+++ map/clif.c
@@ -1380,10 +1380,10 @@ int clif_cutin(struct map_session_data *sd,char *image,int type)
 {
 	int fd=sd->fd;
 
-	WFIFOW(fd,0)=0x145;
-	memcpy(WFIFOP(fd,2),image,16);
-	WFIFOB(fd,18)=type;
-	WFIFOSET(fd,packet_len_table[0x145]);
+	WFIFOW(fd,0)=0x1b3;
+	memcpy(WFIFOP(fd,2),image,64);
+	WFIFOB(fd,66)=type;
+	WFIFOSET(fd,packet_len_table[0x1b3]);
 	return 0;
 }
 
